81b503f117c72364882f2fdbf6c5929c24289d9d
Jorge Garrido <you@example.com>
2023-08-04 14:16:13 -0300

    first commit

---
+++ homebanking/src/main/java/com/mindhub/homebanking/models/Client.java
@@ -0,0 +1,62 @@
+package com.mindhub.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String mail;
+
+    //Constructor vacío para hibernate
+    public Client(){}
+
+    public Client(String firstName, String lastName, String mail){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.mail = mail;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", mail='" + mail + '\'' +
+                '}';
+    }
+}
