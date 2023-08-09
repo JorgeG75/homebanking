212de723c7d246d23c400e5283dde664704e4ab8
Jorge Garrido <you@example.com>
2023-08-09 16:12:53 -0300

    add transactions

--- homebanking/src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ homebanking/src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -41,12 +41,21 @@ public class HomebankingApplication {
 
 
 			Transaction credit1= new Transaction(account1, TransactionType.CREDIT, 1500.0, LocalDateTime.now(),"credit");
-			Transaction debit1t = new Transaction(account2, TransactionType.DEBIT, 1000.0, LocalDateTime.now(), "debit");
+			Transaction debit1 = new Transaction(account1, TransactionType.DEBIT, 1000.0, LocalDateTime.now(), "debit");
+			Transaction credit2= new Transaction(account2, TransactionType.CREDIT, 3000.0, LocalDateTime.now(),"prestamo");
+			Transaction debit2 = new Transaction(account2, TransactionType.DEBIT, 600.0, LocalDateTime.now(), "pago chinos");
+			Transaction credit3= new Transaction(account1, TransactionType.CREDIT, 2500.0, LocalDateTime.now(), "reembolso tarjeta");
 
 			account1.addTransaction(credit1);
-			account2.addTransaction(debit1t);
+			account2.addTransaction(debit1);
+			account1.addTransaction(credit2);
+			account2.addTransaction(debit2);
+			account1.addTransaction(credit3);
 			transactionRepository.save(credit1);
-			transactionRepository.save(debit1t);
+			transactionRepository.save(debit1);
+			transactionRepository.save(credit2);
+			transactionRepository.save(debit2);
+			transactionRepository.save(credit3);
 
 
 //nuevo cliente
@@ -54,12 +63,28 @@ public class HomebankingApplication {
 			System.out.println(client1);
 			clientRepository.save(client1);
 
-			Account newAccount = new Account(client1, "VIN001", 8000.0, LocalDate.now());
+			Account newAccount = new Account(client1, "VIN003", 8000.0, LocalDate.now());
 
 			client1.addAccount(newAccount);
 
 			accountRepository.save(newAccount);
 
+			Transaction newcredit1= new Transaction(newAccount, TransactionType.CREDIT, 1500.0, LocalDateTime.now(),"credit");
+			Transaction newdebit1 = new Transaction(newAccount, TransactionType.DEBIT, 1000.0, LocalDateTime.now(), "debit");
+			Transaction newcredit2= new Transaction(newAccount, TransactionType.CREDIT, 3000.0, LocalDateTime.now(),"prestamo");
+			Transaction newdebit2 = new Transaction(newAccount, TransactionType.DEBIT, 600.0, LocalDateTime.now(), "pago chinos");
+			Transaction newcredit3= new Transaction(newAccount, TransactionType.CREDIT, 2500.0, LocalDateTime.now(), "reembolso tarjeta");
+
+			newAccount.addTransaction(newcredit1);
+			newAccount.addTransaction(newdebit1);
+			newAccount.addTransaction(newdebit2);
+			newAccount.addTransaction(newdebit2);
+			newAccount.addTransaction(newcredit3);
+			transactionRepository.save(newcredit1);
+			transactionRepository.save(newdebit1);
+			transactionRepository.save(newcredit2);
+			transactionRepository.save(newdebit2);
+			transactionRepository.save(newcredit3);
 
 		};
 	}
